78225ae75b9aeb5c2bf7ee762e30b7f179092fa3
makemeunsee <makemeunsee@gmail.com>
2010-10-12 18:47:13 +0200

    try to be standalone then try for user dir

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <QApplication>
-
 #include "mainwindow.h"
+#include <QProcessEnvironment>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QDir dir = QDir::currentPath();
+    QString kanjidic2Path = dir.path() + "/kanjidic2.xml";
+    if(!QFile(kanjidic2Path).exists())
+    {
+#ifdef __WIN32
+        QString userProfile = QProcessEnvironment::systemEnvironment().value("USERPROFILE");
+#else
+        QString userProfile = QProcessEnvironment::systemEnvironment().value("HOME");
+#endif
+        kanjidic2Path = userProfile+"/.kiten+/kanjidic2.xml";
+    }
     MainWindow mainWin;
     mainWin.show();
-    mainWin.open("/home/markus/Documents/kanjidic2_light.xml");
+    mainWin.open(kanjidic2Path);
     return app.exec();
 }
 

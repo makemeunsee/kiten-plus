215e4b3bdf2bc758775b7f03704d88d5529d228f
makemeunsee <makemeunsee@gmail.com>
2010-10-13 14:50:33 +0200

    WIP

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QString kanjidic2File("kanjidic2.xml");
     QDir dir = QDir::currentPath();
-    QString kanjidic2Path = dir.path() + "/kanjidic2.xml";
+    QString kanjidic2Path = dir.path() + "/" + kanjidic2File;
     if(!QFile(kanjidic2Path).exists())
     {
 #ifdef __WIN32
@@ -15,7 +16,7 @@ int main(int argc, char *argv[])
 #else
         QString userProfile = QProcessEnvironment::systemEnvironment().value("HOME");
 #endif
-        kanjidic2Path = userProfile+"/.kiten+/kanjidic2.xml";
+        kanjidic2Path = userProfile+"/.kiten+/"+kanjidic2File;
     }
     MainWindow mainWin;
     mainWin.show();
